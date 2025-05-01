eff14a51d3fe8f839a55af74903fc159ec4c7808
Anastasiya <Lozhkina_Anastasya@list.ru>
2025-05-01 21:54:19 +0300

    add MongoDB configuration with Spring Data support

---
+++ src/main/java/ru/spbstu/hsai/infrastructure/config/MongoConfig.java
@@ -0,0 +1,43 @@
+package ru.spbstu.hsai.infrastructure.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import ru.spbstu.hsai.infrastructure.db.MongoProperties;
+
+@ComponentScan(basePackages = "ru.spbstu.hsai")
+@Configuration
+@PropertySource("classpath:mongo.properties")
+public class MongoConfig  {
+
+    @Bean
+    public MongoProperties mongoProperties(
+            @Value("${mongo.host}") String host,
+            @Value("${mongo.database}") String database) {
+        return new MongoProperties(host, database);
+    }
+
+    @Bean
+    public MongoClient mongoClient(MongoProperties props) {
+        return MongoClients.create(props.host());
+    }
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory(MongoClient client, MongoProperties props) {
+        return new SimpleMongoClientDatabaseFactory(client, props.database());
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory factory) {
+        return new MongoTemplate(factory);
+    }
+}
+
+
