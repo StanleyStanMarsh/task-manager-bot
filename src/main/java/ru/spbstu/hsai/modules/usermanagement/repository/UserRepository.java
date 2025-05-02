92e80bc192125f23ca2838a528add1e7bfe52e4e
Anastasiya <Lozhkina_Anastasya@list.ru>
2025-05-02 20:49:48 +0300

    add reactivity

--- src/main/java/ru/spbstu/hsai/modules/usermanagement/repository/UserRepository.java
+++ src/main/java/ru/spbstu/hsai/modules/usermanagement/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package ru.spbstu.hsai.modules.usermanagement.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 import ru.spbstu.hsai.modules.usermanagement.model.User;
 
-import java.util.Optional;
-
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByTelegramId(Long telegramId);  // Поиск пользователя по Telegram ID
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
+    Mono<User> findByTelegramId(Long telegramId);
 }
