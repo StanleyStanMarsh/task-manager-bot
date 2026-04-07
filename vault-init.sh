ac283971ca4fa77cf9d362d69bbcf15b469e37d2
Pluzhnikova Julia <pluzhnikova.yus@edu.spbstu.ru>
2026-04-07 15:37:57 +0300

    changes for lab4

--- vault-init.sh
+++ vault-init.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-VAULT_TOKEN=replace_this_text_with_token
+export VAULT_ADDR=http://vault:8200
+VAULT_TOKEN=replace_me
 export VAULT_TOKEN
 
-echo "Vault is ready. Initializing and loading secrets..."
+sleep 3
 
-# Enable key-value storage
-vault secrets enable -version=2 -path=secret kv
+vault secrets enable -version=2 -path=secret kv || true
 
-# Load secrets
 vault kv put secret/task-manager-bot \
-  mongo.host="" \
-  mongo.port="" \
-  mongo.database="" \
-  mongo.username="" \
-  mongo.password="" \
-  telegram.bot.token="" \
-  telegram.bot.username="" \
-  superadmin.telegramId="" \
-  superadmin.username="" \
-  superadmin.firstName="" \
-  superadmin.lastName="" \
-  superadmin.password=""
+  mongo.host="mongo" \
+  mongo.port="27017" \
+  mongo.database="mydb" \
+  mongo.username="$MONGO_USERNAME" \
+  mongo.password="$MONGO_PASSWORD" \
+  telegram.bot.token="$TELEGRAM_BOT_TOKEN" \
+  telegram.bot.username="$TELEGRAM_BOT_USERNAME" \
+  superadmin.telegramId="123456789" \
+  superadmin.username="super_admin" \
+  superadmin.firstName="admin" \
+  superadmin.lastName="admin" \
+  superadmin.password="password"
 
-echo "Secrets loaded successfully!"
+echo "Secrets loaded!"
